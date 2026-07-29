60985395103ab5b49cc7716359394b2d940b6c29
Michael Isaza <maicolisaza06@gmail.com>
2026-07-28 23:28:26 -0500

    Add Rol model and migration

---
+++ backend/src/migrations/20260729040600-create-rol.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('roles', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.ENUM('admin', 'user'),
+        allowNull: false,
+        unique: true,
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('roles');
+  },
+};
