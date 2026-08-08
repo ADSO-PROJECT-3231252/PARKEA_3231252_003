9bc8a5100692d00ca4d37109914ad23ece6e6749
davidleon1089gme-oss <samueldavidleonpescadorgmail.com>
2026-08-07 21:41:21 -0500

    mount admin routes under /api/admin

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,5 +6,6 @@ router.use('/zones', require('./zona.routes'));
 router.use('/reservations', require('./reserva.routes'));
 router.use('/payments', require('./pago.routes'));
 router.use('/users', require('./usuario.routes'));
+router.use('/admin', require('./admin.routes'));
 
 module.exports = router;
